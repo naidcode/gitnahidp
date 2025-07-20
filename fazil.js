3513486e5381adca18de674f0564f6dd005cca62
naidcode <naidcode.dev@gamil.com>
2025-07-20 23:18:58 +0530

    version12

--- fazil.js
+++ fazil.js
@@ -1,2 +1,9 @@
 console.log("hello guys ");
 console.log("version11");
+let a = 154;
+
+if (a > 100){
+    console.log("you know ")
+} else {
+    console.log("you don't know")
+}
--- fouz.js
+++ fouz.js
@@ -1,2 +1,3 @@
 console.log("changes for ever young ");
 console.log("version11");
+console.log("nahid pasha ")
